d97a551604f055356fa8abf196aedb5cf2095d7c
m6ydz642 <45617447+m6ydz642@users.noreply.github.com>
2020-08-18 17:24:37 +0900

    로그아웃 추가 & 로그인 부분 회원가입과 동일한 디자인 적용

---
+++ src/net/customer/action/CustomerLogoutAction.java
@@ -0,0 +1,62 @@
+package net.customer.action;
+
+import java.io.PrintWriter;
+import java.util.logging.Logger;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.sun.corba.se.spi.orbutil.fsm.Action;
+import com.sun.corba.se.spi.orbutil.fsm.FSM;
+import com.sun.corba.se.spi.orbutil.fsm.Input;
+
+import action.ActionForward;
+import net.customer.db.CustomerBean;
+import net.customer.db.CustomerDAO;
+
+
+
+public class CustomerLogoutAction implements Action {
+	
+	private final static Logger log = Logger.getGlobal();
+
+	
+
+	public ActionForward execute(HttpServletRequest req, HttpServletResponse resp) throws Exception {
+		
+		HttpSession session=req.getSession();
+			
+		session.invalidate();
+
+		System.out.println("로그아웃 처리 함수 호출");
+
+		PrintWriter out=resp.getWriter();
+		resp.setCharacterEncoding("UTF-8");
+	    resp.setContentType("text/html;charset=UTF-8"); 
+		 
+		out.println("<script>");
+		out.println("alert('안녕히가세요');");
+		out.println("location.href='./index.jsp';");
+		out.println("</script>");
+		out.close();
+	
+		ActionForward forward = new ActionForward();
+	
+		return null;
+		
+	} // method
+
+
+
+	@Override
+	public void doIt(FSM arg0, Input arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+
+
+} // class
