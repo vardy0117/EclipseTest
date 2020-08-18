ebed52d1cf2618869c3aebf276ab4d36d04f94a9
mimi <68892026+dkzld159@users.noreply.github.com>
2020-08-18 19:36:36 +0900

    0818 bean 생성

---
+++ src/net/menu/db/MenuBean.java
@@ -0,0 +1,32 @@
+package net.menu.db;
+
+public class MenuBean {
+	
+	private String  storeNo;
+	private String name;
+	private String price;
+	
+	
+	//getter,setter
+	public String getStoreNo() {
+		return storeNo;
+	}
+	public void setStoreNo(String storeNo) {
+		this.storeNo = storeNo;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPrice() {
+		return price;
+	}
+	public void setPrice(String price) {
+		this.price = price;
+	}
+	
+	
+	
+}
---
+++ src/net/store/db/StoreBean.java
@@ -0,0 +1,101 @@
+package net.store.db;
+
+public class StoreBean {
+
+	private String storeNo;
+	private String ceoNo;
+	private String name;
+	private String address;
+	private String category;
+	private String phone;
+	private String storeHours;
+	private String message;
+	private String image;
+	private String points;
+	private String orederCount;  
+	private String deliveryArea;
+	private String regNo;
+	
+	
+	public String getStoreNo() {
+		return storeNo;
+	}
+	public void setStoreNo(String storeNo) {
+		this.storeNo = storeNo;
+	}
+	public String getCeoNo() {
+		return ceoNo;
+	}
+	public void setCeoNo(String ceoNo) {
+		this.ceoNo = ceoNo;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getStoreHours() {
+		return storeHours;
+	}
+	public void setStoreHours(String storeHours) {
+		this.storeHours = storeHours;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public String getImage() {
+		return image;
+	}
+	public void setImage(String image) {
+		this.image = image;
+	}
+	public String getPoints() {
+		return points;
+	}
+	public void setPoints(String points) {
+		this.points = points;
+	}
+	public String getOrederCount() {
+		return orederCount;
+	}
+	public void setOrederCount(String orederCount) {
+		this.orederCount = orederCount;
+	}
+	public String getDeliveryArea() {
+		return deliveryArea;
+	}
+	public void setDeliveryArea(String deliveryArea) {
+		this.deliveryArea = deliveryArea;
+	}
+	public String getRegNo() {
+		return regNo;
+	}
+	public void setRegNo(String regNo) {
+		this.regNo = regNo;
+	}
+	
+	
+	
+}
