23c93af1fe154fe7c056a6bdc69440de59274988
m6ydz642 <45617447+m6ydz642@users.noreply.github.com>
2020-08-18 19:05:07 +0900

    로그인 암호 기능 삭제

--- src/net/customer/db/CustomerDAO.java
+++ src/net/customer/db/CustomerDAO.java
@@ -137,7 +137,7 @@ public class CustomerDAO {
 	} // method
 /***********************************************************************************************/
 	public boolean CheckCustomer(CustomerBean cb) { // 로그인 검사 
-		
+	
 		boolean result = false;
 		
 
@@ -146,33 +146,38 @@ public class CustomerDAO {
 			 getConnection();
 			 
 //			sql ="select aes_decrypt(unhex(password), ?) as email from customer where email = ? ";
-			 sql ="select ? from customer where email = ? ";
-			// mysql에 aes 암호 알고리즘(?) 사용 해서 원본 아이디 비밀번호 검사
+			 sql ="select password from customer where email = ? ";
 
+			 
 
 			 pstmt = con.prepareStatement(sql);
-			 pstmt.setString(1, cb.getPassword());
-			 pstmt.setString(2, cb.getEmail());
+			  pstmt.setString(1, cb.getEmail());
+			
 			 
 			// rs.getString("email");
 			 
 			 
 			 rs = pstmt.executeQuery();		 
-			 rs.next();
+
+if (rs.next()) {
+	result = true;
+	System.out.println("로그인 성공");
+}
 	
+			 
 //			 if(rs.getString("email") != null) { // 쿼리가 정상적으로 작동하면 실행, 암호가 다른상태로 select문 되었을때 안맞으면 
 				 								// 무조건 null이 나옴  비밀번호 틀린것으로 간주,   
 				 								// 그리고 암호화가 안된 데이터, 
 				 								// 즉 개발자가 임의로 넣은 테스트 데이터는 이 검사 명령이 안먹힘
 				 	
-		if(rs.next() == true)		  {
+/*		if(rs.next() == true)		  {
 				 System.out.println("유저 DB내용 : " +  cb.getEmail() );
 				 
 				 System.out.println("비밀번호 : " + cb.getPassword() + " 아이디 : " + cb.getEmail());
 				 System.out.println("아이디 검사 성공! 사용자가 정보를 맞게 입력함");
 				 System.out.println("검사 성공 rs.next내용" + rs.next());
 				 
-				/* *//************************************************************************************//*
+				 *//************************************************************************************//*
 				String sql2 = "select * from customer where email=?";
 				pstmt = con.prepareStatement(sql2);
 				pstmt.setString(1, cb.getEmail());
@@ -188,7 +193,7 @@ public class CustomerDAO {
 				// 로그인시 정보 select 해서 가져오기
 				System.out.println("customer dao에서 가져온 닉네임 :  " + cb.getNickname());
 				System.out.println("customer dao에서 가져온 customerNo :  " + cb.getCustomerNo());
-				*//************************************************************************************/
+				*//************************************************************************************//*
 					 
 				 result = true;
 			 }else{
@@ -196,7 +201,7 @@ public class CustomerDAO {
 				 System.out.println("비밀번호 : " + cb.getPassword() + " 아이디 : " + cb.getEmail());
 				 System.out.println("검사 실패 rs.next내용" + rs.next());
 				 result = false;
-			 }
+			 }*/
 			
 		} catch (Exception e){
 			System.out.println("CheckCustomer메소드 내부에서 예외 발생 : " + e);
