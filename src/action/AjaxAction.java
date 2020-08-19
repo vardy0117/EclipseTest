ad236adfd239d706d0402aa4db822fecfb021dd9
devjean1115 <68333443+devjean1115@users.noreply.github.com>
2020-08-19 12:47:17 +0900

    Update AjaxAction.java

--- src/action/AjaxAction.java
+++ src/action/AjaxAction.java
@@ -3,9 +3,14 @@ package action;
 
 import java.io.PrintWriter;
 
+
+import org.json.simple.JSONObject;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+
+import net.customer.db.CustomerBean;
 import net.customer.db.CustomerDAO;
 
 public class AjaxAction {
@@ -24,34 +29,6 @@ public class AjaxAction {
 	} // method
 	
 	public void emailCheckFromCeo(HttpServletRequest request, HttpServletResponse response) throws Exception {
-
-		request.setCharacterEncoding("UTF-8");
-
-		String email = request.getParameter("email");
-		String result = "notUsable";
-
-		CustomerDAO cdao = new CustomerDAO();
-			result = cdao.joinCheckEmail(email);
-		
-		PrintWriter out = response.getWriter();
-		out.print(result);
-	} // method
-}
-
-package action;
-
-import java.io.PrintWriter;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import org.json.simple.JSONObject;
-
-import net.customer.db.CustomerBean;
-import net.customer.db.CustomerDAO;
-
-public class AjaxAction {
-	public void emailCheck(HttpServletRequest request, HttpServletResponse response) throws Exception {
 
 		request.setCharacterEncoding("UTF-8");
 
@@ -84,4 +61,3 @@ public class AjaxAction {
 		return jsonObj.toJSONString();
 	}
 }
-
