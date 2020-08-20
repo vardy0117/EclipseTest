8239bd6fe25ac6796ce9a0e513d3d85504611705
m6ydz642 <45617447+m6ydz642@users.noreply.github.com>
2020-08-20 16:13:00 +0900

    사장님 로그인 컨트롤러 수정

--- src/controller/FrontController.java
+++ src/controller/FrontController.java
@@ -247,6 +247,33 @@ public class FrontController extends HttpServlet {
 			forward.execute(request, response);
 		}		
 	
+		if(command.equals("CeoLoginAction.do")) {
+			CeoLoginAction action = new CeoLoginAction() ;
+			boolean result = false;
+			try {
+				 forward = new ActionForward();
+				 result = action.execute(request, response);
+				 if(result){
+					 forward.setRedirect(true);
+					 forward.setView("ceoIndex.jsp"); // 사장님 전용페이지가 없어서 일단 여기로 했습니당
+					 System.out.println("사장님 로그인 리다이렉트 작동 " + forward.getView());
+				 } else {
+					 PrintWriter out = response.getWriter();
+					 response.setContentType("text/html;charset=UTF-8"); 
+					 
+					 out.println("<script>"); 
+					 out.println("alert('사업자 로그인에 실패하셨습니다. \\n 아이디와 비밀번호 확인 후 다시 로그인해주세요.');"); 
+					 out.println("history.back();"); 
+					 out.println("</script>");
+					System.out.println("사업자 로그인 실패 result값 " + result);
+				 }
+
+				forward.execute(request, response);
+				
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}		
 		
 
 		
