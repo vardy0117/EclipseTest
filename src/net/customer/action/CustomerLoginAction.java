7fbd4db2bb1021e4ae83b269b9563c2dd2728dbc
devjean1115 <68333443+devjean1115@users.noreply.github.com>
2020-08-20 15:43:27 +0900

    customerLoginAction, frontcontroller 수정 내용 : 경로 세팅, 방식 수정

--- src/net/customer/action/CustomerLoginAction.java
+++ src/net/customer/action/CustomerLoginAction.java
@@ -17,7 +17,7 @@ public class CustomerLoginAction {
 	
 	private final static Logger log = Logger.getGlobal();
 	
-	public ActionForward execute(HttpServletRequest req, HttpServletResponse resp) throws Exception {
+	public boolean execute(HttpServletRequest req, HttpServletResponse resp) throws Exception {
 		
 		log.info("CustomerLoginAction execute()");
 		System.out.println("로그인 액션");
@@ -75,23 +75,12 @@ public class CustomerLoginAction {
 				
 				System.out.println("세션 등록 완료 ");
 				// 페이지 이동 설정 (이동 방식, 경로)
-				
-				forward.setRedirect(true);
-				forward.setView("./");
-				System.out.println("로그인 리다이렉트 작동 " + forward.getView());
 			
 		}else{
 			
 			 resp.setCharacterEncoding("UTF-8");
-			 PrintWriter out = resp.getWriter();
-			 resp.setContentType("text/html;charset=UTF-8"); 
-			 
-			 out.println("<script>"); 
-			 out.println("alert('로그인에 실패하셨습니다. \\n 아이디와 비밀번호 확인 후 다시 로그인해주세요.');"); 
-			 out.println("history.back();"); 
-			 out.println("</script>");
-			System.out.println("로그인 실패 result값 " + result);
-			return null;
+			
+			
 		
 		}
 		
@@ -101,7 +90,7 @@ public class CustomerLoginAction {
 	
 		
 
-		return forward;
+		return result;
 		
 	} // method
 
